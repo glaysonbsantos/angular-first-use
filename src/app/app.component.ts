8059699f6b23f17ec2f25e0f6a5f384a650a5e8d
Glayson <glaysonbss@gmail.com>
2022-08-05 10:26:44 -0300

    Comunicação entre componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,12 +12,27 @@ import { Component, OnInit } from '@angular/core';
       <p>Esse conteúdo foi inserido via ng-content!</p>
       <hr />
     </app-diretivas-atributos> -->
-
-    <app-header></app-header>
+    <!-- <app-header></app-header> -->
+    <app-input [contador]="value"></app-input>
+    <button class="btn" (click)="addValue()">+</button>
+    <button class="btn" (click)="subValue()">-</button>
+    <button class="btn" (click)="clearValue()">AC</button>
     <router-outlet></router-outlet>
   `,
 })
 export class AppComponent implements OnInit {
+  public value: number = 0;
+
   constructor() {}
   ngOnInit(): void {}
+
+  public addValue() {
+    this.value++;
+  }
+  public subValue() {
+    this.value--;
+  }
+  public clearValue() {
+    this.value = 0;
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, InputComponent],
   imports: [CommonModule],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent, InputComponent],
 })
 export class SharedModule {}
